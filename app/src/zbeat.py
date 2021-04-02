36d31f494a4097cb5abe2e5163134adf44a7550c
ElDiabloRojo <holdens.uk@gmail.com>
2021-04-02 20:56:13 +0100

    code format

--- app/src/zbeat.py
+++ app/src/zbeat.py
@@ -20,17 +20,19 @@ UPDATE_PERIOD = 300
 
 hr = HeartRate(max30105)
 heart_rate = prometheus_client.Gauge('heart_rate_bpm',
-                              'Depth of water in water tank. Full = 2.7m',
-                              ['location'])
+                                     'Depth of water in water tank. Full = 2.7m',
+                                     ['location'])
+
 
 def publish_heartrate(beat, bpm, avg_bpm):
     heart_rate.labels('beat').set(beat)
     heart_rate.labels('bpm').set(bpm)
     heart_rate.labels('avg_bpm').set(avg_bpm)
 
+
 if __name__ == '__main__':
-  prometheus_client.start_http_server(9999)
-  
+    prometheus_client.start_http_server(9999)
+
 while True:
-  hr.on_beat(publish_heartrate, average_over=4)
-  time.sleep(UPDATE_PERIOD)
+    hr.on_beat(publish_heartrate, average_over=4)
+    time.sleep(UPDATE_PERIOD)
